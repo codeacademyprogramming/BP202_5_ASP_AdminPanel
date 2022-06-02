0f1962502531cd6bf319924033f465f0ecf54ef3
eaghayev <eaghayevv@gmail.com>
2022-06-02 21:22:28 +0400

    project added

---
+++ Pustok/Pustok/Areas/Manage/Controllers/AuthorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pustok.DAL;
+using System.Linq;
+
+namespace Pustok.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class AuthorController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AuthorController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Authors.Include(x=>x.Books).ToList();
+            return View(data);
+        }
+    }
+}
---
+++ Pustok/Pustok/Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pustok.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string FullName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public List<Book> Books { get; set; }
+    }
+}
